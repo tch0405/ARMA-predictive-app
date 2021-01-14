6db4c21bc702bfea6dd187f1e75bf0944989c268
tch0405 <45081337+tch0405@users.noreply.github.com>
2021-01-14 12:30:03 +0800

    Add files via upload

---
+++ flask1.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify, render_template
+import joblib
+import traceback
+import pandas as pd
+import numpy as np
+
+
+
+# Your API definition
+app = Flask(__name__)
+@app.route('/')
+def index():
+  return render_template('index.html')
+  
+@app.route('/predict', methods=['POST'])
+def predict():
+    if lr:
+        try:
+            value=1100
+            int_features = [int(x) for x in request.form.values()]
+            value = int_features[0]
+            #json_ = request.json
+            #print(json_)
+            #value = int(json_['value'])
+
+            prediction = list(lr.predict(value,value))
+
+            #return jsonify({'Prediction for SR at Peak Demand (MW)': str(prediction)})
+            return render_template('index.html', prediction_text='Next SR at Peak Demand (MW) should be {}MW'.format(prediction[0]))
+        
+        except:
+            return
+
+        
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
+
+if __name__ == '__main__':
+    
+    port = 8888
+    lr = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model columns loaded')
+
+    app.run(port=port, debug=True)
